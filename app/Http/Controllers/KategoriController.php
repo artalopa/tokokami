dd6cd56a90c2ba7d2c215c152b930cd3a8bd4557
artalopa <ryanartalopa@gmail.com>
2023-05-09 15:12:57 +0700

    penggunaan eloquent orm dan beberapa perbaikan bug

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Kategori;
 
 class KategoriController extends Controller
 {
@@ -12,8 +13,8 @@ class KategoriController extends Controller
         $title = 'Kategori';
 
         // mengambil data dari tabel produks
-        // $produk = DB::table('kategoris')->join('produks', 'kategoris.id', '=', 'produks.kategori_id')->get();
-        $kategori = DB::table('kategoris')->get();
+        // $kategori = DB::table('kategoris')->get();   //query builder
+        $kategori = Kategori::all();   //eloquent orm
 
         // mengirim data ke view admin->kategori
         return view('admin.kategori', ['dataKategori' => $kategori, 'title' => $title]);
@@ -22,10 +23,15 @@ class KategoriController extends Controller
     public function store(Request $request)
     {
         // insert data ke tabel kategori
-        DB::table('kategoris')->insert([
+        /*DB::table('kategoris')->insert([
             'nama_kategori' => $request->nama_kategori,
             'deskripsi' => $request->deskripsi,
-        ]);
+        ]); */
+        $kategori = new Kategori;
+        $kategori->nama_kategori = $request->nama_kategori;
+        $kategori->deskripsi = $request->deskripsi;
+        $kategori->save();
+        
 
         // alihkan halaman ke halaman kategori
         return redirect('/admin/kategori');
@@ -34,10 +40,14 @@ class KategoriController extends Controller
     public function update(Request $request)
     {
         // insert data ke tabel kategori
-        DB::table('kategoris')->where('id', $request->id)->update([
+        /*DB::table('kategoris')->where('id', $request->id)->update([
             'nama_kategori' => $request->nama_kategori,
             'deskripsi' => $request->deskripsi,
-        ]);
+        ]); */
+        $kategori = Kategori::where('id', $request->id)->first();
+        $kategori->nama_kategori = $request->nama_kategori;
+        $kategori->deskripsi = $request->deskripsi;
+        $kategori->save();
 
         // alihkan halaman ke halaman kategori
         return redirect('/admin/kategori');
@@ -46,7 +56,8 @@ class KategoriController extends Controller
     public function delete(Request $request)
     {
         // hapus data tabel kategoris
-        DB::table('kategoris')->where('id', $request->id)->delete();
+        // DB::table('kategoris')->where('id', $request->id)->delete();
+        Kategori::where('id', $request->id)->delete();
 
         // alihkan halaman ke halaman kategori
         return redirect('/admin/kategori');
--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Kategori;
+use App\Models\Produk;
 
 class ProdukController extends Controller
 {
@@ -13,7 +15,7 @@ class ProdukController extends Controller
 
         // mengambil data dari tabel produks
         $produk = DB::table('kategoris')->join('produks', 'kategoris.id', '=', 'produks.kategori_id')->get();
-        $kategori = DB::table('kategoris')->get();
+        $kategori = Kategori::all();  
 
         // mengirim data ke view admin->produk
         return view('admin.produk', ['dataProduk' => $produk, 'dataKategori' => $kategori, 'title' => $title]);
@@ -21,18 +23,17 @@ class ProdukController extends Controller
 
     public function store(Request $request)
     {
-        // $request->file('gambar')->store('uploads');
+        // simpan file gambar ke folder storage
         $fileGambar = $request->file('gambar')->store('uploads');
 
-        // Simpan nama file ke database
         // insert data ke tabel produks
-        DB::table('produks')->insert([
-            'nama_produk' => $request->nama_produk,
-            'deskripsi' => $request->deskripsi,
-            'harga' => $request->harga,
-            'gambar' => $fileGambar,
-            'kategori_id' => $request->kategori_id,
-        ]);
+        $produk = new Produk;
+        $produk->nama_produk = $request->nama_produk;
+        $produk->deskripsi = $request->deskripsi;
+        $produk->harga = $request->harga;
+        $produk->gambar = $fileGambar;
+        $produk->kategori_id = $request->kategori_id;
+        $produk->save();
 
         // alihkan halaman ke halaman produk
         return redirect('/admin/produk');
@@ -40,20 +41,20 @@ class ProdukController extends Controller
 
     public function update(Request $request)
     {
-      if($request->file('gambar')) {
-        $fileGambar = $request->file('gambar')->store('uploads');
-        DB::table('produks')->where('id', $request->id)->update([
-            'gambar' => $fileGambar,
-        ]);
-      }
+        $produk = Produk::where('id', $request->id)->first();
+      
+        if($request->file('gambar')) {
+          $fileGambar = $request->file('gambar')->store('uploads');
+        
+          $produk->gambar = $fileGambar;
+        }
       
         // update data tabel produks
-        DB::table('produks')->where('id', $request->id)->update([
-            'nama_produk' => $request->nama_produk,
-            'deskripsi' => $request->deskripsi,
-            'harga' => $request->harga,
-            'kategori_id' => $request->kategori_id,
-        ]);
+        $produk->nama_produk = $request->nama_produk;
+        $produk->deskripsi = $request->deskripsi;
+        $produk->harga = $request->harga;
+        $produk->kategori_id = $request->kategori_id;
+        $produk->save();
 
         // alihkan halaman ke halaman produk
         return redirect('/admin/produk');
@@ -62,7 +63,7 @@ class ProdukController extends Controller
     public function delete(Request $request)
     {
         // hapus data tabel produks
-        DB::table('produks')->where('id', $request->id)->delete();
+        Produk::where('id', $request->id)->delete();
 
         // alihkan halaman ke halaman produk
         return redirect('/admin/produk');
